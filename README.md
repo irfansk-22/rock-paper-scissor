f5c9c0eb6426b04986deac032063a46fd399a4fb
Irfan <irfansk.se@gmail.com>
2020-08-10 19:54:00 +0530

    refactored code and added readme file

---
+++ README.md
@@ -0,0 +1,18 @@
+![RPS Game Home Page]()
+
+# Rock Paper Scissor
+
+Cool Rock Paper Scissor game you can play with your friends for deciding who will do work ;)
+
+In case if you don't know, Rock paper scissors is a hand game played between two people, in which each player simultaneously forms one of three shapes with an outstretched hand. These shapes are "rock", "paper", and "scissors". [know more about this game](https://en.wikipedia.org/wiki/Rock_paper_scissors) 
+  
+## Technologies Used 
+
+- HTML 5
+- CSS 3
+- Vanila JavaScript
+
+## About this project
+
+- On clicking on play button the game generates random options (either rock, paper or scissor) for player 1 and player 2 and according to the game logic one of the player wins.
+- This game also has a feature of editing players name.
--- index.js
+++ index.js
@@ -1,38 +1,50 @@
 var p1 = "player 1";
 var p2 = "player 2";
 
+var selectP1 = document.querySelector(".player1");
+var selectP2 = document.querySelector(".player2");
+var selectImg1 = document.querySelector(".img1");
+var selectImg2 = document.querySelector(".img2");
+
+//get dynamic year for copyright
+var currentYear = document.querySelector(".year");
+var year = new Date().getFullYear();
+currentYear.innerHTML = year;
+
+//function for editing names
 function editNames() {
 
     p1 = prompt("Player 1, What is your Name?");
     p2 = prompt("Player 2, What is your Name?");
 
-    var check1 = ((p1.length === 0) && (p2.length === 0));
-    var check2 = ((p1.length > 0) && (p2.length === 0));
-    var check3 = ((p1.length === 0) && (p2.length > 0));
+    const check1 = ((p1.length === 0) && (p2.length === 0));
+    const check2 = ((p1.length > 0) && (p2.length === 0));
+    const check3 = ((p1.length === 0) && (p2.length > 0));
 
     if (check1) {
-        document.querySelector(".player1").innerHTML = "player 1";
-        document.querySelector(".player2").innerHTML = "player 2";
+        selectP1.innerHTML = "player 1";
+        selectP2.innerHTML = "player 2";
     } else if (check2) {
-        document.querySelector(".player1").innerHTML = p1;
-        document.querySelector(".player2").innerHTML = "player 2";
+        selectP1.innerHTML = p1;
+        selectP2.innerHTML = "player 2";
     } else if (check3) {
-        document.querySelector(".player1").innerHTML = "player 1";
-        document.querySelector(".player2").innerHTML = p2;
+        selectP1.innerHTML = "player 1";
+        selectP2.innerHTML = p2;
     } else {
-        document.querySelector(".player1").innerHTML = p1;
-        document.querySelector(".player2").innerHTML = p2;
+        selectP1.innerHTML = p1;
+        selectP2.innerHTML = p2;
     }
 }
 
+//The game logic goes in this function
 function play() {
 
     const stone = 1, paper = 2, scissor = 3;
 
     var randomNumber1 = Math.floor((Math.random() * 3) + 1);
     var randomNumber2 = Math.floor((Math.random() * 3) + 1);
-    document.querySelector(".img1").setAttribute("src", "images/rps" + randomNumber1 + ".png");
-    document.querySelector(".img2").setAttribute("src", "images/rps" + randomNumber2 + ".png");
+    selectImg1.setAttribute("src", "images/rps" + randomNumber1 + ".png");
+    selectImg2.setAttribute("src", "images/rps" + randomNumber2 + ".png");
 
     if (randomNumber1 == stone && randomNumber2 == paper) {
         player2Wins();
@@ -58,19 +70,19 @@ function play() {
 
 function player1Wins() {
     document.querySelector("h1").innerHTML = (p1 + " Wins! 🚩");
-    document.querySelector(".img1").classList.add("winner");
-    document.querySelector(".img2").classList.remove("winner");
+    selectImg1.classList.add("winner");
+    selectImg2.classList.remove("winner");
 }
 
 function player2Wins() {
     document.querySelector("h1").innerHTML = (p2 + " Wins! 🚩");
-    document.querySelector(".img2").classList.add("winner");
-    document.querySelector(".img1").classList.remove("winner");
+    selectImg2.classList.add("winner");
+    selectImg1.classList.remove("winner");
 }
+
 
 function draw() {
     document.querySelector("h1").innerHTML = " Draw! 🤝";
-    document.querySelector(".img1").classList.remove("winner");
-    document.querySelector(".img2").classList.remove("winner");
+    selectImg1.classList.remove("winner");
+    selectImg2.classList.remove("winner");
 }
-
